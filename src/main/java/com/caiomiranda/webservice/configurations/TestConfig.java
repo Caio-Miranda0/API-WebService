c6440df68a898ebcfc4714905b995cbf1224c1a0
Caio Charles <caio.miranda@estudante.ifb.edu.br>
2024-12-24 12:45:53 -0300

    Entidade Category + Repository + Service + Resource + Povoar banco de dados com carga de teste

--- src/main/java/com/caiomiranda/webservice/configurations/TestConfig.java
+++ src/main/java/com/caiomiranda/webservice/configurations/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.caiomiranda.webservice.entities.Category;
 import com.caiomiranda.webservice.entities.Order;
 import com.caiomiranda.webservice.entities.User;
 import com.caiomiranda.webservice.entities.enums.OrderStatus;
+import com.caiomiranda.webservice.repositories.CategoryRepository;
 import com.caiomiranda.webservice.repositories.OrderRepository;
 import com.caiomiranda.webservice.repositories.UserRepository;
 
@@ -32,6 +34,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		User user1 = new User(null, "Larissa", "larissa@gmail.com", "99999", "123");
@@ -43,6 +48,12 @@ public class TestConfig implements CommandLineRunner {
 		Order order3 = new Order(null, Instant.parse("2024-12-23T08:00:00Z"), OrderStatus.WAITING_PAYMENT, user1);
 
 		orderRepository.saveAll(List.of(order1, order2, order3));
+
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+
+		categoryRepository.saveAll(List.of(cat1, cat2, cat3));
 	}
 
 }
