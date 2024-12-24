87be79d18ccc91c908c3409135ff76538d72c7e9
Caio Charles <caio.miranda@estudante.ifb.edu.br>
2024-12-23 21:33:15 -0300

    Usei a anotação @JsonFormat para formatar a saída do horário no padrão ISO 8601 quando da consulta ao banco de dados

--- src/main/java/com/caiomiranda/webservice/entities/Order.java
+++ src/main/java/com/caiomiranda/webservice/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +22,8 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	// Formata a saída no JSON no padrão ISO 8691
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	//private OrderStatus orderStatus;
 	
